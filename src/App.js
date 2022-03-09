30a942f124a7d8592c2bf1a9b4bd1646da802268
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-09 12:36:48 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import theme from './theme/theme';
+import { Home } from './home';
+import { Survey } from './survey';
+import { NewSurvey } from './CreateSurvey/SurveyForm';
+import { Box, Button, Flex, ChakraProvider } from '@chakra-ui/react';
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link as RouteLink,
+} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <ChakraProvider theme={theme}>
+            <Router>
+                <Flex>
+                    <RouteLink to="/">
+                        <Button m={3}>Home</Button>
+                    </RouteLink>
+                </Flex>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/survey" element={<Survey />} />
+                    <Route path="/createSurvey" element={<NewSurvey />} />
+                </Routes>
+            </Router>
+        </ChakraProvider>
     );
 }
 
