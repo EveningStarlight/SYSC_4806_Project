f79bfb058408894ed79eebeecc78b2bd3090761f
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-09 12:37:13 -0500

    Develop landing page

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import { VStack, Box, Button } from '@chakra-ui/react';
+import { Frame } from './frame';
+import { Link as RouteLink } from 'react-router-dom';
+
+function Home() {
+    return (
+        <Frame title="Survey Gorilla">
+            <Box>The goto place for all your survey needs</Box>
+            <VStack m={3}>
+                <RouteLink to="/createSurvey">
+                    <Button m={3}>New Survey</Button>
+                </RouteLink>
+                <RouteLink to="/survey">
+                    <Button m={3}>Complete Survey</Button>
+                </RouteLink>
+            </VStack>
+        </Frame>
+    );
+}
+
+export { Home };
