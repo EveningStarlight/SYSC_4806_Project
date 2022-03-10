569b61894508d7469a05c05b5033f65da83eec3e
Jayton McCracken <32139643+jaymccracken@users.noreply.github.com>
2022-03-10 18:13:30 -0500

    Answer survey (#27)
    * Button and file moving
    
    moving files in to folders
    
    andding a submit button on answer survey
    
    * forgot import
    
    * Answer Survey Submit
    
    function to get the user input for each question on submit of form
    
    * removal of ()
    
    * Route on submit to the home page
    
    * import statment needed
    
    * run prettier

---
+++ src/AnswerSurvey/survey.js
@@ -0,0 +1,36 @@
+import { Box, VStack, Input, Button } from '@chakra-ui/react';
+import { Frame } from '../frame';
+import { Link as RouteLink } from 'react-router-dom';
+
+var survey = require('../surveys.json');
+
+function Survey() {
+    return (
+        <Frame title={survey.title}>
+            <form id="AnswerSurveyForm" onSubmit={answerSurveySubmit}>
+                <VStack m={3}>
+                    <Box>{survey.description}</Box>
+                    <Box id="1">
+                        {survey.Q1}
+                        <Input type="text" placeholder={survey.Q1} mb={2} />
+
+                        <RouteLink to="/">
+                            <Button type="submit" value="submit">
+                                Answer Survey
+                            </Button>
+                        </RouteLink>
+                    </Box>
+                </VStack>
+            </form>
+        </Frame>
+    );
+}
+
+export { Survey };
+
+function answerSurveySubmit() {
+    for (let i = 1; i < survey.numOfQs + 1; i++) {
+        let answer = document.getElementById(i.toString);
+        console.log(answer);
+    }
+}
