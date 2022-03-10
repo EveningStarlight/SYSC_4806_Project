53e1e5fa4562b0c9d3ab39c7318e065de49e166a
mattman555 <1999mrh@gmail.com>
2022-03-10 16:26:31 -0500

    require json instead of fetch

--- src/survey.js
+++ src/survey.js
@@ -3,33 +3,13 @@ import { Frame } from './frame';
 import React, { useState, useEffect } from 'react';
 
 function Survey() {
-	const [data,setData]=useState([]);
-	const getData=()=>{
-	fetch('namesurvey.json'
-	,{
-	  headers : { 
-		'Content-Type': 'application/json',
-		'Accept': 'application/json'
-	   }
-	)
-	  .then(function(response){
-		console.log(response)
-		return response.json();
-	  })
-	  .then(function(myJson) {
-		console.log(myJson);
-		setData(myJson)
-	  });
-	}
-	useEffect(()=>{
-	getData()
-	},[])
+	var survey = require('./namesurvey.json');
     let title = 'Survey Title';
     let desc = 'a place to answer questions';
 
     return (
-        <Frame title={data.title}>
-            <Box>{data.description}</Box>
+        <Frame title={survey.title}>
+            <Box>{survey.description}</Box>
         </Frame>
     );
 }
