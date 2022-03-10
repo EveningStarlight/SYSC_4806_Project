8bbc25a28b34ddf561175acdc1517773acb2d551
Jayton McCracken <jmccrac@bell.net>
2022-03-10 17:40:47 -0500

    Answer Survey Submit
    function to get the user input for each question on submit of form

--- src/AnswerSurvey/survey.js
+++ src/AnswerSurvey/survey.js
@@ -1,24 +1,34 @@
 import { Box, VStack, Input, Button} from '@chakra-ui/react';
 import { Frame } from '../frame';
 
+var survey = require('../surveys.json');
+
 function Survey() {
-    var survey = require('../surveys.json');
 
     return (
         <Frame title={survey.title}>
-            <VStack m={3}>
-                <Box>{survey.description}</Box>
-                <Box>
-                    {survey.Q1}
-                    <Input type="text" placeholder={survey.Q1} mb={2} />
+            <form id="AnswerSurveyForm" onSubmit={answerSurveySubmit()}>
+                <VStack m={3}>
+                    <Box>{survey.description}</Box>
+                    <Box id="1">
+                        {survey.Q1}
+                        <Input type="text" placeholder={survey.Q1} mb={2} />
 
-                    <Button type="submit" value="submit">
-                            Answer Survey
-                    </Button>
-                </Box>
-            </VStack>
+                        <Button type="submit" value="submit">
+                                Answer Survey
+                        </Button>
+                    </Box>
+                </VStack>
+            </form>
         </Frame>
     );
 }
 
 export { Survey };
+
+function answerSurveySubmit(){
+    for (let i=1; i < survey.numOfQs + 1; i++){
+        let answer = document.getElementById(i.toString);
+        console.log(answer);
+    } 
+}
