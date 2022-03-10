1ff19ee510063e6502360675b30155b08315c35d
mattman555 <1999mrh@gmail.com>
2022-03-10 15:35:21 -0500

    added reading json file

--- src/survey.js
+++ src/survey.js
@@ -2,12 +2,34 @@ import { Box } from '@chakra-ui/react';
 import { Frame } from './frame';
 
 function Survey() {
+	const [data,setData]=useState([]);
+	const getData=()=>{
+	fetch('namesurvey.json'
+	,{
+	  headers : { 
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	   }
+	}
+	)
+	  .then(function(response){
+		console.log(response)
+		return response.json();
+	  })
+	  .then(function(myJson) {
+		console.log(myJson);
+		setData(myJson)
+	  });
+	}
+	useEffect(()=>{
+	getData()
+	},[])
     let title = 'Survey Title';
     let desc = 'a place to answer questions';
 
     return (
-        <Frame title={title}>
-            <Box>{desc}</Box>
+        <Frame title={data.title}>
+            <Box>{data.description}</Box>
         </Frame>
     );
 }
