07fc0be9234f33e2415cec0d1d226c6f65fadd57
Jayton McCracken <jmccrac@bell.net>
2022-03-10 18:18:41 -0500

    Reformating
    moving around where files are

--- src/Database/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "mysql:host=localhost;dbname=minisurveymonkey";
-$username = "user";
-$password = 'password';
-
-// Create connection
-$pdo = new PDO($servername,$username,$password);
-
-?>
