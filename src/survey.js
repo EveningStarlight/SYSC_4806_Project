5b6ef97fa04f2c7edaca60d16380b18b2a3b1be3
Matthew Harris <1999mrh@gmail.com>
2022-03-10 16:51:35 -0500

    Read file (#26)
    * added browserfs
    
    * added reading json file
    
    * removed browserfs from package json
    
    * add imports
    
    * fetch from git
    
    * fetch from git user content
    
    * fetch from github with no cors
    
    * fetch from github with no cors
    
    * fetch from github with no cors
    
    * remove github fetching
    
    * require json instead of fetch
    
    * add input
    
    * add input import
    
    * run prettier
    
    * remove unused imports

--- src/survey.js
+++ src/survey.js
@@ -1,13 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, VStack, Input } from '@chakra-ui/react';
 import { Frame } from './frame';
 
 function Survey() {
-    let title = 'Survey Title';
-    let desc = 'a place to answer questions';
+    var survey = require('./namesurvey.json');
 
     return (
-        <Frame title={title}>
-            <Box>{desc}</Box>
+        <Frame title={survey.title}>
+            <VStack m={3}>
+                <Box>{survey.description}</Box>
+                <Box>
+                    {survey.Q1}
+                    <Input type="text" placeholder={survey.Q1} mb={2} />
+                </Box>
+            </VStack>
         </Frame>
     );
 }
