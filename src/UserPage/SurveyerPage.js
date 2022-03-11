c78931f1e521878620184bc70f022260a3dda34e
Imran Latif <wolf2184@gmail.com>
2022-03-10 22:05:08 -0500

    Testing multiple tables showing up

--- src/UserPage/SurveyerPage.js
+++ src/UserPage/SurveyerPage.js
@@ -9,19 +9,28 @@ import {
 } from '@chakra-ui/react';
 import { Frame } from '../frame';
 var database = require('../surveys.json');
+var list = database.tables
 
 
 function DisplaySurveys() {
 	
     return (
 		<Frame title="Your Surveys:">
-			
 			<Stack direction="row" justifyContent="space-between">
 				<p>{database.title}</p>
 				<p>{database.description}</p>
-				
+				<p>{list.length}</p>
 			</Stack>
-			
+		// {
+			// for (let i = 0; i < database.tables; i++) {
+		
+			// <Stack direction="row" justifyContent="space-between">
+				// <p>{database.title}</p>
+				// <p>{database.description}</p>
+				
+			// </Stack>
+			// }
+		// }
 		</Frame>
 	);
 }
