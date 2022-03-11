5adf56e94962746d555a590a45a4142fd30bef88
Jayton McCracken <32139643+jaymccracken@users.noreply.github.com>
2022-03-11 08:52:20 -0500

    Testcases (#33)
    * Survey.Test
    
    testing if the page properly redirects to the answer survey page
    
    * SurveyForm.test
    
    Test cases for Redirecting to the page
    Test case for adding new questions
    Test case for Removing a question
    
    Yarn updated with new import libraries
    
    * Prettier
    
    i ran npm prettier:
    fixing all of the code to meet standard

---
+++ src/AnswerSurvey/survey.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Survey } from './survey';
+
+test('Redirect to Answer Survey Form Page', () => {
+    render(<Survey />, { wrapper: MemoryRouter });
+    const linkElement = screen.getByText(/a simple survey asking your name!/i);
+    expect(linkElement).toBeInTheDocument();
+});
