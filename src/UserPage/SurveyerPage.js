8f7f75cf97400c7ac5f6dd9e56f4100a70c45212
Imran Latif <wolf2184@gmail.com>
2022-03-11 08:54:50 -0500

    Fixing Errors

--- src/UserPage/SurveyerPage.js
+++ src/UserPage/SurveyerPage.js
@@ -11,30 +11,16 @@ import { Frame } from '../frame';
 var database = require('../surveys.json');
 var list = database.tables
 
-function getTableInfo(){
-	
-	for(var i = 0; i < (list.length); i++) {
-		document.createElement(<Box>);
-			document.createElement(<Stack  direction="row" justifyContent="space-between">);
-				document.createElement(<p>{list[i].title}</p>);
-				document.createElement(<p>{list[i].description}</p>);
-			document.createElement(</Stack>);
-		document.createElement(</Box>);
-	}
-	
-	
-	
-}
+
 function DisplaySurveys() {
 	
     return (
 		<Frame title="Your Surveys:">
 			<div id="tables">
-			//	{ window.onload = () =>{ document.getElementById("tables").appendChild( getTableInfo )}};
-			<Stack direction="row" justifyContent="space-between">
-				<p>{list[0].title}</p>
-				<p>{list[0].description}</p>
-			</Stack>
+				<Stack direction="row" justifyContent="space-between">
+					<p>{list[0].title}</p>
+					<p>{list[0].description}</p>
+				</Stack>
 			</div>
 		</Frame>
 	);
@@ -44,8 +30,23 @@ function DisplaySurveys() {
 export { DisplaySurveys };
 
 //Testing multiple tables showing up
+//	{ window.onload = () =>{ document.getElementById("tables").appendChild( getTableInfo )}};
 			// <Stack direction="row" justifyContent="space-between">
 				// <p>{database.title}</p>
 				// <p>{database.description}</p>
 				// <p>{list.length}</p>
 			// </Stack>
+// function getTableInfo(){
+	
+	// for(var i = 0; i < (list.length); i++) {
+		// document.createElement(<Box>);
+			// document.createElement(<Stack  direction="row" justifyContent="space-between">);
+				// document.createElement(<p>{list[i].title}</p>);
+				// document.createElement(<p>{list[i].description}</p>);
+			// document.createElement(</Stack>);
+		// document.createElement(</Box>);
+	// }
+	
+	
+	
+// }
