3e23b176ff889922420a0c8bf62dd8c712ac190c
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-10 22:05:43 -0500

    remove QL constructor

--- src/CreateSurvey/Questions/QuestionList.js
+++ src/CreateSurvey/Questions/QuestionList.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class QuestionList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return <>{this.props.questions}</>;
     }
