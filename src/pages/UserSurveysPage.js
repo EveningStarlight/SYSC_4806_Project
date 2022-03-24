1e22b8a9966a1f9895d3a5d540933329bffd64d3
Imran Latif <wolf2184@gmail.com>
2022-03-24 17:37:32 -0400

    Update UserSurveysPage.js

--- src/pages/UserSurveysPage.js
+++ src/pages/UserSurveysPage.js
@@ -1,15 +1,15 @@
 import { Frame } from '../components/frame';
 import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  Button,
-} from '@chakra-ui/react'
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Button,
+} from '@chakra-ui/react';
 var database = require('../database/data.json');
 var list = database.tables;
 
@@ -17,42 +17,34 @@ function DisplaySurveys() {
     return (
         <Frame title="Your Surveys:">
             <div id="tables">
-				<Table>
-					<Thead> 
-					
-						<Tr>
-							<Td>
-								Survey Names
-							</Td>
-							<Td>
-								Survey Descriptions
-							</Td>
-							<Td style={{textAlign: 'center'}}>
-								Survey Details 
-							</Td>
-						</Tr>
-					
-					</Thead>
-					<Tbody >
+                <Table>
+                    <Thead fontWeight='bold'>
+                        <Tr>
+                            <Td>Survey Names</Td>
+                            <Td>Survey Descriptions</Td>
+                            <Td style={{ textAlign: 'center' }}>
+                                Survey Details
+                            </Td>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr justifycontent="space-evenly">
+                            <Td>
+                                <p>{database.title}</p>
+                            </Td>
+                            <Td>
+                                <p>{database.description}</p>
+                            </Td>
+                            <Td style={{ textAlign: 'center' }}>
+                                <Button m={3} colorScheme="purple">
+                                    See Details
+                                </Button>
+                            </Td>
+                        </Tr>
 
-						<Tr justifycontent="space-evenly">
-							<Td><p>{database.title}</p></Td>
-							<Td><p>{database.description}</p></Td>
-							<Td style={{textAlign: 'center'}}>
-								<Button m={3} colorScheme="purple" >
-									See Details
-								</Button>
-							</Td>
-						</Tr>
-			
-			
-						 
-						{/* Now to account for multiple entires. */}
-
-			
-			
-					</Tbody>
-				</Table>
+                        {/* Now to account for multiple entires. */}
+                    </Tbody>
+                </Table>
             </div>
         </Frame>
     );
