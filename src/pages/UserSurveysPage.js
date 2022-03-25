8cb28198a6e74c1034f94badd3a92f0b3578adae
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-24 20:43:15 -0400

    remove base html tags

--- src/pages/UserSurveysPage.js
+++ src/pages/UserSurveysPage.js
@@ -5,36 +5,28 @@ var database = require('../database/data.json');
 function DisplaySurveys() {
     return (
         <Frame title="Your Surveys:">
-            <div id="tables">
-                <Table variant="striped">
-                    <Thead>
-                        <Tr>
-                            <Th fontSize="lg">Survey Names</Th>
-                            <Th fontSize="lg">Survey Descriptions</Th>
-                            <Th fontSize="lg" style={{ textAlign: 'center' }}>
-                                Survey Details
-                            </Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr justifycontent="space-evenly">
-                            <Td>
-                                <p>{database.title}</p>
-                            </Td>
-                            <Td>
-                                <p>{database.description}</p>
-                            </Td>
-                            <Td style={{ textAlign: 'center' }}>
-                                <Button m={3} colorScheme="purple">
-                                    See Details
-                                </Button>
-                            </Td>
-                        </Tr>
-
-                        {/* Now to account for multiple entires. */}
-                    </Tbody>
-                </Table>
-            </div>
+            <Table variant="striped">
+                <Thead>
+                    <Tr>
+                        <Th fontSize="lg">Survey Names</Th>
+                        <Th fontSize="lg">Survey Descriptions</Th>
+                        <Th fontSize="lg" style={{ textAlign: 'center' }}>
+                            Survey Details
+                        </Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr justifycontent="space-evenly">
+                        <Td>{database.title}</Td>
+                        <Td>{database.description}</Td>
+                        <Td style={{ textAlign: 'center' }}>
+                            <Button m={3} colorScheme="purple">
+                                See Details
+                            </Button>
+                        </Td>
+                    </Tr>
+                </Tbody>
+            </Table>
         </Frame>
     );
 }
