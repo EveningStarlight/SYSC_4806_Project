c859ea6db6a7c3313765746fdf8a7abd81a91cb8
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-04-05 11:42:56 -0400

    Query db (#75)
    * add mongodb
    
    * try querying database
    
    * Update UserSurveysPage.js
    
    * moved import
    
    * get data to console
    
    * survey list from db
    
    * change get
    
    * usde state
    
    * single fetch call
    
    Co-authored-by: Matthew Harris <1999mrh@gmail.com>

--- src/pages/UserSurveysPage.js
+++ src/pages/UserSurveysPage.js
@@ -1,25 +1,44 @@
-import { Frame } from '../components/frame';
+import { useState, useEffect } from 'react';
 import { Table, Thead, Tbody, Tr, Th, Td, Button } from '@chakra-ui/react';
 import { Link as RouteLink } from 'react-router-dom';
-var database = require('../database/data.json');
+import axios from 'axios';
+
+import { Frame } from '../components/frame';
+import { LoadingMessage } from '../components/LoadingMessage';
 
 function DisplaySurveys() {
+    const [surveyList, setSurveyList] = useState(null);
+    useEffect(() => {
+        getAllSurveys().then((data) => {
+            setSurveyList(data);
+        });
+    }, []);
     return (
         <Frame title="Your Surveys:">
-            <Table variant="striped">
-                <Thead>
-                    <Tr>
-                        <Th fontSize="lg">Survey Names</Th>
-                        <Th fontSize="lg">Survey Descriptions</Th>
-                        <Th fontSize="lg" style={{ textAlign: 'center' }}>
-                            Survey Details
-                        </Th>
-                    </Tr>
-                </Thead>
-                <Tbody>{renderRows(database.surveys)}</Tbody>
-            </Table>
+            {!surveyList ? (
+                <LoadingMessage>Surveys</LoadingMessage>
+            ) : (
+                <Table variant="striped">
+                    <Thead>
+                        <Tr>
+                            <Th fontSize="lg">Survey Names</Th>
+                            <Th fontSize="lg">Survey Descriptions</Th>
+                            <Th fontSize="lg" style={{ textAlign: 'center' }}>
+                                Survey Details
+                            </Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>{renderRows(surveyList)}</Tbody>
+                </Table>
+            )}
         </Frame>
     );
+}
+
+function getAllSurveys() {
+    return axios.get('/api/surveys').then((surveys) => {
+        return surveys.data;
+    });
 }
 
 function renderRows(surveys) {
