faedb8506dc165458adc4585b867332130fafa5a
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-04-04 20:02:00 -0400

    single fetch call

---
+++ src/components/LoadingMessage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Spinner, Stack, Text } from '@chakra-ui/react';
+
+function LoadingMessage({ children }) {
+    return (
+        <Stack align="center" layerStyle="loadingMessage">
+            <Stack isInline align="center">
+                <Spinner
+                    size="lg"
+                    speed="0.6s"
+                    color="primary"
+                    emptyColor="accent"
+                    thickness="0.175em"
+                />
+                <Text>Loading {children}...</Text>
+            </Stack>
+        </Stack>
+    );
+}
+
+export { LoadingMessage };
