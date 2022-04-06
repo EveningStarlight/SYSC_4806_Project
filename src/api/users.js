80a852313cca360e58f9f861a167fb8dfb88f889
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-04-06 18:03:15 -0400

    implement crypto

--- src/api/users.js
+++ src/api/users.js
@@ -4,30 +4,42 @@ const Crypto = require('crypto');
 
 const User = require('../models/user');
 
-
-
 router.get('/', (req, res) => {
     User.find()
         .then((users) => res.json(users))
         .catch((err) => console.log(err));
 });
 
-router.post('/', (req, res) => {
-    const { email, password } = req.body;
-    const salt = Crypto.randomBytes(16);
-    const newUser = new User({
-        email: email, password: password
-        //salt: salt,
-        //passwordHash: Crypto.createHmac('sha256', password + salt),
-    })
-    newUser.save()
-        .then(() => res.json({
-            message: "Created account successfully"
-        }))
-        .catch(err => res.status(400).json({
-            "error": err,
-            "message": "Error creating account"
-        }))
+router.post('/:type', (req, res) => {
+    const type = req.params.type;
+    if (type === 'createUser') {
+        createUser(req, res);
+    }
 });
+
+const createUser = (req, res) => {
+    const { email, password } = req.body;
+    const salt = Crypto.randomBytes(16).toString('hex');
+    const hmac = Crypto.createHmac('sha256', salt);
+    const hash = hmac.update(password);
+    const newUser = new User({
+        email: email,
+        salt: salt,
+        passwordHash: hmac.digest('hex'),
+    });
+    newUser
+        .save()
+        .then(() =>
+            res.json({
+                message: 'Created account successfully',
+            }),
+        )
+        .catch((err) =>
+            res.status(400).json({
+                error: err,
+                message: 'Error creating account',
+            }),
+        );
+};
 
 module.exports = router;
