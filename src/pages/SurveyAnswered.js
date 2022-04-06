47b036f3fa373dc364b33b0404d8d048e48c6fcb
Matthew Harris <1999mrh@gmail.com>
2022-04-06 14:25:03 -0400

    Now able to go back to survey or answer another one (#80)
    * Now able to go back to survey or answer another one
    
    * prettier

--- src/pages/SurveyAnswered.js
+++ src/pages/SurveyAnswered.js
@@ -2,18 +2,19 @@ import { Box, VStack, Button } from '@chakra-ui/react';
 import { Frame } from '../components/frame';
 import { Link as RouteLink } from 'react-router-dom';
 function SurveyAnswered() {
+    let title = document.cookie;
     return (
         <Frame title="Thank You!">
             <Box m={3}>Thank You for filling out the survey!</Box>
             <Box m={3}>Your reponse has been saved.</Box>
 
             <VStack m={3}>
-                <RouteLink to="/survey">
+                <RouteLink to={'/survey/' + title}>
                     <Button m={3} colorScheme="purple">
                         Submit Another Response
                     </Button>
                 </RouteLink>
-                <RouteLink to="/">
+                <RouteLink to="/survey">
                     <Button m={3} colorScheme="purple">
                         Answer A Different Survey
                     </Button>
